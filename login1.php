77a628ed9473dc5fced4bca74fe5391f24330c0a
emrearslan2 <50243951+emrearslan2@users.noreply.github.com>
2019-05-16 01:45:00 +0300

    Create login1.php

---
+++ login1.php
@@ -0,0 +1,46 @@
+<?php include("baglan.php"); ?>
+<html>
+<body bgcolor="#F34723"><center>
+<font color="#98FF98"><h1>Kullanici Girisi</h1></font>
+<form action="" method="POST">
+                            <input type="text" name="email" placeholder="Email adresinizi giriniz...">
+                            <input type="password" name="password" placeholder="Şifre">
+                            <div class="form-button"><br>
+                                <button id="submit" type="submit" ><font color="#F34723">Giris</font></button>
+                            </div>
+
+                            </body>
+                            </html>
+                            <?php include("baglan.php"); ?><br><br><br>
+
+
+
+
+<?php
+session_start();
+
+ob_start();
+if($_POST){
+    $sql = "select * from girisler where email='$_POST[email]' and password='$_POST[password]'";
+    $sorgu = mysqli_query($baglanti, $sql);
+    if (mysqli_num_rows($sorgu) >= 1) {
+        $_SESSION["login"] = "true";
+
+        $_SESSION["email"] = $email;
+
+        $_SESSION["password"] = $password;
+        header("Location: /proje/index1.html");
+    }
+    else{
+        echo "kullanıcı bilgileri eşleşmedi";
+    }
+}
+
+?>
+
+
+
+
+
+
+</center>
