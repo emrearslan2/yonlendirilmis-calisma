b244dbe74b3ae9663779f5d187f4b9fe674c9704
emrearslan2 <50243951+emrearslan2@users.noreply.github.com>
2019-06-18 22:53:11 +0300

    Create neffooter.php

---
+++ neffooter.php
@@ -0,0 +1,19 @@
+ <div id="footer-sec">
+        &copy; 2019  | Emre Arslan : <a href="http://www.binarytheme.com/" target="_blank">BinaryTheme.com</a>
+    </div>
+    <!-- /. FOOTER  -->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+    <script src="/proje/assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS -->
+    <script src="/proje/assets/js/bootstrap.js"></script>
+    <!-- METISMENU SCRIPTS -->
+    <script src="/proje/assets/js/jquery.metisMenu.js"></script>
+       <!-- CUSTOM SCRIPTS -->
+    <script src="/proje/assets/js/custom.js"></script>
+    
+
+
+</body>
+</html>
+
