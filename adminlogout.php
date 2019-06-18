15fc3c6466ac8775e75baf4372495df3345a1abb
emrearslan2 <50243951+emrearslan2@users.noreply.github.com>
2019-06-18 22:51:44 +0300

    Create adminlogout.php

---
+++ adminlogout.php
@@ -0,0 +1,25 @@
+<?php
+
+ 
+
+session_start();
+
+ 
+
+ob_start();
+
+ 
+
+session_destroy();
+
+ 
+
+echo "Çıkış Yaptınız.Ana Sayfaya Yönlendiriliyorsunuz";
+
+ 
+
+header("Refresh: 2; url=/proje/ygris.php");
+
+ 
+
+?>
