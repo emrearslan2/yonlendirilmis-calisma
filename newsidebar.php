fa03f4653c21436114f7f3f8725f96b1b836bc3b
emrearslan2 <50243951+emrearslan2@users.noreply.github.com>
2019-06-18 22:56:02 +0300

    Create newsidebar.php

---
+++ newsidebar.php
@@ -0,0 +1,30 @@
+       <!-- /. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+                    <li>
+                        <div class="user-img-div">
+                            <img src="assets/img/user.png" class="img-thumbnail" />
+
+                            <div class="inner-text">
+                                Jhon Deo Alex
+                            <br />
+                                <small>Last Login : 2 Weeks Ago </small>
+                            </div>
+                        </div>
+
+                    </li>
+
+
+                    <li>
+                        <a class="active-menu" href="index.html"><i class="fa fa-dashboard "></i>Anasayfa</a>
+                    </li>
+                  
+                    
+                 
+                   
+                     
+                   
+            </div>
+
+        </nav>
